f961dac8374008b0818bb3760fdfedee3368e6f4
dani <dani.zaghian@gmail.com>
2021-01-21 14:09:41 -0800

    adds w03 whitebaording

---
+++ w3.rb
@@ -0,0 +1,12 @@
+# Write a function that takes in an array of numbers and returns the largest number.
+
+# clarify
+# input: [2, 5, 3, 88, 34, 1, 88] => array
+# output: 88 => integer
+
+# visualize (how do I do this?)
+# find first number in the array & assign to a variable
+# loop through the array and for each element, see if it's larger than the variable
+# make variable whichever number is larger
+# ouput final variable of largest number
+
