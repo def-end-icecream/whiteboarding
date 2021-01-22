77b1c8b26343511ba63422c17b56c7ef0006bbc6
dani <dani.zaghian@gmail.com>
2021-01-22 13:28:25 -0800

    outlines whiteboarding process

--- w3.rb
+++ w3.rb
@@ -10,3 +10,26 @@
 # make variable whichever number is larger
 # ouput final variable of largest number
 
+# verification
+# array
+# [2, 5, 3, 88, 34, 1, 88]
+# largest = 88
+
+# code
+def max_num(numbers)
+  largest = numbers[0]
+  numbers.each do |number|
+    largest = number if number > largest
+  end
+  largest
+end
+
+p max_num([2, 5, 3, 88, 34, 1, 88])
+p max_num([90, -1, -22, 0])
+
+# debug
+# this solves the original problem and produces the correct output
+
+# refactor
+# O(n)
+# no efficiency improvments that we're aware of
