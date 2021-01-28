fa3fe031a6911fa0361c62c20bc5a697978a95b4
dani <dani.zaghian@gmail.com>
2021-01-28 13:44:42 -0800

    adds w04 decomp

---
+++ w4.rb
@@ -0,0 +1,11 @@
+# Write a function that takes in two strings and returns the longest common substring.
+
+# clarify
+# "racecar" "car", "together" "bothersome" => 2 strings
+# "car", "ther" => string
+
+# visualize
+# write a method that prints out "hello"
+# write a method that takes in 2 strings and prints out each one
+# write a function that takes in 2 strings and prints out the letters of the first string one line at a time, and then prints out the letters of the second string one line at a time
+# write a function that takes in 2 strings and loops through each letter of the first string and compare each letter to each letter in the second string and print "match" if they match
