111a351a6d37e6c9737a154822512e9286751d5f
dani <dani.zaghian@gmail.com>
2021-01-28 14:01:38 -0800

    adds decompositions for second problem

--- w4.rb
+++ w4.rb
@@ -9,3 +9,15 @@
 # write a method that takes in 2 strings and prints out each one
 # write a function that takes in 2 strings and prints out the letters of the first string one line at a time, and then prints out the letters of the second string one line at a time
 # write a function that takes in 2 strings and loops through each letter of the first string and compare each letter to each letter in the second string and print "match" if they match
+
+
+
+
+# Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
+# example input [1, 1]
+# example output: [1]
+
+# write a function that prints "hello"
+# write a function that takes in an array of numbers and prints the array of numbers
+# write a function that takes in an array of numbers and loops through the array and prints each number
+# write a function that takes in an array of numbers and loops through each number, with another inner loop to compare current number with each other number and print "match" if they match
