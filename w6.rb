89ddf74f2e61e158ddbf7483fdcd669e1d1b3e7d
dani <dani.zaghian@gmail.com>
2021-02-11 13:47:38 -0800

    decomp w6

---
+++ w6.rb
@@ -0,0 +1,26 @@
+# Write a function that takes in a string of code with parentheses, square brackets, and curly brackets, and checks if the code is balanced with correct bracket syntax.
+
+# clarify
+# example input: "{}{()}[]([)]"
+# expected output: false
+# example input: "(x + [1, {a: 3, b:2}] + y)"
+# expected output: true
+
+# visualize/decompose
+# write a function that counts how many parenthesis, curly braces, and square brackets appear in the string
+# write a function that takes in a string and counts how many open parenthesis and how many close parenthesis there are (do the same for curly braces and square brackets)
+
+
+# Write a function that takes in two arrays of numbers and returns all the numbers that appear in both arrays.
+
+# clarify
+# example input: [4, 2, 16, 95, 3, 5] [7, 3, 90, 2, 95]
+# {4: true, 2: true, 16: true, 95: true, 3: true, 5: true}
+# example output: [3, 2, 95]
+
+# visualize/decompose
+# Write a function that takes in 2 arrays and prints each of them out
+# Write a function that takes in 2 arrays and loops through one array and pushes each value into a new array
+# Write a function that takes in 2 arrays and loops through the first array and compares each number to the first number in the second array
+# Write a function that  takes in 2 arrays and loops through the first one, and then loops through the second (nested) and compares each number in the first array to each number in the second
+# Write a function that takes in 2 arrays and loops through the first array and keeps track of if the number appears in a hash
